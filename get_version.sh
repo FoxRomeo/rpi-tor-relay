58f8ae3b7401f37a9874ae26aa4eb834fb72c217
Oliver Faßbender <git@intrepid.de>
2021-02-12 07:48:22 +0000

    changed build process

--- get_version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#https://www.torproject.org/download/tor/
-#<td class="text-right"><a href="https://dist.torproject.org/tor-0.4.1.6.tar.gz">Download</a>
-VERSION=`curl -s "https://www.torproject.org/download/tor/" | grep -E '\"https://dist.torproject.org/tor-+[0-9]\.+[0-9]\.+[0-9]\.+[0-9].tar.gz\"' |  sed -n -e 's/^.*<td class=\"text-right\"><a href=\"https:\/\/dist\.torproject\.org\/tor-'//p | sed -n -e 's/\.tar\.gz\">Download<\/a>.*$'//p | head -n 1`
-
-echo ${VERSION}
